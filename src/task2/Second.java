1c7b3b1648f389b91b7bf0466862aec94e1ffb6a
Surv <mega.andrej2013@gmail.com>
2019-11-10 13:34:45 +0300

    final...

--- src/task2/Second.java
+++ src/task2/Second.java
@@ -1,4 +1,73 @@
 package task2;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Second {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long nod = Math.abs(scanner.nextLong());
+        long nok = Math.abs(scanner.nextLong());
+
+        long DK = nod * nok;
+        if (nod ==0 || nok % nod != 0 || nok ==0) {
+            System.out.println(0);
+            return;
+        }
+        long xy = nok / nod;
+  //      System.out.println("xy = " + xy);
+        long tempa, tempb;
+        int count =0;
+        for (int i = 1; i <= xy; i++) {
+  //          System.out.println(((func(xy, i))) + " i = " + i);
+            long temp = func(xy, i)*nod;
+            if (NOD(temp, i*nod) == nod ) {
+           //     System.out.println(temp + "  :  " + i);
+
+                count++;
+            }
+
+        }
+        System.out.println(count);
+    }
+
+    public static long func(long z, long x) {
+        if (z % x == 0)
+        return z/x;
+        return 0;
+    }
+
+    private static long NOD(long num1,long num2) {
+        long min;
+        long nod = 0;
+        if(num1 > num2)
+            min = num2;
+        else
+            min = num1;
+
+        for (long count = 1; count <= min; count++) {
+            if (num1 % count == 0 && num2 % count == 0){
+                if (count>nod)
+                    nod = count;
+            }
+        }
+        return nod;
+    }
+    private static long NOK(long num1,long num2) {
+        long count;
+        long nok = 0;
+        if (num1 < num2)
+            count = num2;
+        else
+            count = num1;
+
+        while (nok == 0) {
+            if (count % num1 == 0 && count % num2 == 0)
+                nok = count;
+            count++;
+        }
+        return nok;
+    }
 }
--- src/task3/Third.java
+++ src/task3/Third.java
@@ -1,4 +1,63 @@
 package task3;
 
+
+/*В первой строке задано число T (1 ≤ T ≤ 1000) – количество тестов. Каждый тест состоит из двух строк.
+
+В первой строке каждого теста задано число n (2 ≤ n ≤ 106) – количество лежаков.
+
+Во второй строке каждого теста заданы n чисел ai (1 ≤ i ≤ n, 0 ≤ ai ≤ 109) – значения,
+которые были поставлены лежакам в соответствие по внешним признакам.
+
+Сумма n по всем тестам не превосходит 106.*/
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Third {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        long[][] lehakov = new long[106][1000];
+
+
+        long t = scanner.nextLong();
+
+        for (int k= 0; k< t; k++) {
+              lehakov[k][0] = scanner.nextLong();
+
+            for (int i = 1; i < lehakov[k][0]+1; i++) {
+                lehakov[k][i] = scanner.nextLong();
+            }
+
+        }
+
+        long min = 2147483647L;
+        long temp;
+
+        for (int k= 0; k< t; k++) {
+
+            for (int i = 1; i < lehakov[k][0]; i+=1) {
+
+                temp = lehakov[k][i] ^ lehakov[k][i+1];
+        //        System.out.println("temp = " + temp);
+
+                if (temp < min) {
+                    min =  temp;
+                }
+
+            }
+            System.out.println(min);
+
+        }
+
+
+
+    }
+
+
 }
+
